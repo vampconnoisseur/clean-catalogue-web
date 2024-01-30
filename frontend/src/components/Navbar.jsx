567d4ecc624c699627c5bb95a2cedc418129e3da
HARSH SHUKLA <HARSHSHUKLA2022@VITBHOPAL.AC.IN>
2024-01-30 23:19:27 +0530

    UI: created the landing page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import { SignInButton, SignOutButton, useAuth } from "@clerk/clerk-react";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   const { isSignedIn } = useAuth();
+
   return (
-    <div className="px-16 w-full h-16 flex justify-between items-center bg-red-100">
+    <div className="relative px-16 w-full h-16 flex justify-between items-center bg-blue-500">
       <div>
-        <h1>LOGOG</h1>
+        <img
+          src={logo}
+          alt="logo"
+          className="logo w-48 cursor-pointer"
+          onClick={() => (window.location.href = "/")}
+        />
       </div>
-      <div>
+      <div className="relative">
         {isSignedIn ? (
-          <SignOutButton />
+          <div className="relative">
+            <SignOutButton className="py-2 px-4 bg-white text-black rounded-md shadow-inner" />
+          </div>
         ) : (
           <SignInButton
             afterSignInUrl="/dashboard"
             afterSignUpUrl="/dashboard"
+            className="py-2 px-4 bg-white text-black rounded-md shadow-inner"
           />
         )}
       </div>
